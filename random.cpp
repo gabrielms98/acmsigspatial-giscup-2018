8e1714ed116c42a2fedb694237f529c90a12e4d9
Gabriel M Silva <gabriel.silva@nobugs.com.br>
2018-11-19 17:14:51 -0200

    NAO VAI SER CLONADO

--- random.cpp
+++ random.cpp
@@ -6,6 +6,45 @@
 
 using namespace std;
 
+void tarjanUT(int u, const vector<list<int> > &adj, vector<int> &dfsParent, vector<int> &dfsNum,
+					 vector<int> &dfsLow, vector<bool> &isArticulation,	int &dfsCount, int &rootChildren, int dfsRoot) {
+	dfsLow[u] = dfsNum[u] = dfsCount++; // dfsLow[u] <= dfsNum[u]
+	for(list<int>::const_iterator it = adj[u].begin(); it!=adj[u].end(); it++) {
+		//vertices nao descobertos tem tempo de descoberto (dfsNum) 0
+		if (dfsNum[(*it)] == 0) {
+				dfsParent[(*it)] = u; //pai de v e' u (v foi descoberto por u)
+			if (u == dfsRoot) //caso especial... precisamos contar quantas vezes descobrimos vertices a partir da raiz para descobrir se raiz e' articulacao...
+				rootChildren++;
+
+			tarjanUT((*it),adj,dfsParent,dfsNum,dfsLow,isArticulation,dfsCount,rootChildren,dfsRoot);
+
+			if (dfsLow[(*it)] >= dfsNum[u]) //apos visitar v recursivamente, conseguimos chegar em um vertice em v ou antes passando por u? (sem voltar pela aresta v,u)
+				isArticulation[u] = true; // store this information first
+			dfsLow[u] = min(dfsLow[u], dfsLow[(*it)]); // update dfsLow[u]
+		}
+		else if ((*it) != dfsParent[u]) //cheguei de volta em um vertice ja descoberto... se nao passei pela aresta v,u de volta --> ciclo...
+			dfsLow[u] = min(dfsLow[u], dfsNum[(*it)]); //atualize dfsLow...
+	}
+}
+
+void tarjan(const vector<list<int> > &adj, vector<bool> &isArticulation) {
+	int nv = adj.size();
+	vector<int> dfsParent(nv,-1);
+	vector<int> dfsLow(nv,0);
+	vector<int> dfsNum(nv,0);
+	int dfsCount = 0;
+
+	//para multiplos CCs ...
+	for(int i=0; i<nv; i++) {
+		if(dfsNum[i]==0) {
+			int rootChildren = 0;//resete para cada CC!!
+			tarjanUT(i,adj,dfsParent,dfsNum,dfsLow,isArticulation,dfsCount,rootChildren,i);
+			isArticulation[i] = rootChildren>1;
+		}
+	}
+}
+
+
 void geraGrafo(Graph &g, int v, int e) {
 	g = Graph(v);
 	srand(time(NULL));
@@ -25,12 +64,12 @@ int main(int argc, char **argv){
 
 	vector<bool>start(v);
 	vector<bool>control(v);
-	vector<bool>sol(v); for(int i=0; i<v; i++){sol[i] = false;control[i] = false;start[i] = false;}
+	vector<bool>sol(v); for(int i=0; i<v; i++){sol[i] = false; control[i] = false; start[i] = false;}
 
-	start[1] = true;
+	/*start[1] = true;
 	control[v-1] = true;
-	control[v-3] = true;
-	for(int i=0; i<v; i++) if(i%2==0)sol[i]=true;
+	control[v-3] = true;*/
+	tarjan(g.adj, sol);
 
 	g.save(argv[1], start, control, sol);
 }
