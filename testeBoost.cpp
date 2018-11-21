54f4cb1a62a1efe7d2fd708659042776a8d0e87a
Gabriel M Silva <gabriel.silva@nobugs.com.br>
2018-11-21 21:09:28 -0200

    makefie

---
+++ testeBoost.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <boost/graph/adjacency_list.hpp>
+
+using namespace std;
+using namespace boost;
+
+typedef adjacency_list<listS, vecS> Graph; // VertexList=vecS
+
+int main(){
+
+  Graph g;
+
+}
