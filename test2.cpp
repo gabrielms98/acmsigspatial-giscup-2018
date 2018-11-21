3aedd28f33a17bd9e5b2f9899cd4273964866293
RicsonV <ricsonluiz@yahoo.com.br>
2018-11-21 13:36:17 -0200

    tacale pau

--- test2.cpp
+++ test2.cpp
@@ -12,14 +12,14 @@ typedef boost::adjacency_list<boost::listS,boost::vecS,boost::undirectedS> Graph
 typedef boost::graph_traits<Graph>::vertex_descriptor vertex;
 typedef boost::graph_traits<Graph>::edge_iterator edge_iterator;
 
-void saveG(const Graph &g, const string &file, vector<bool> &startBool, const vector<bool>&controlBool, const vector<bool>&arti){
+void saveG(const Graph &g, const string &file, const set<int> &startSet, const set<int>&controlSet, const set<int>&artiSet, int V){
 	  ofstream f(file.c_str(),ios::out);
 		f << "graph G{ " << '\n';
-		for(int i=0;i<startBool.size();i++){
+		for(int i=0;i<V;i++){
 			f << i;
-      if(arti[i]) f << "[color=\".7 .3 1.0\"]" << "[style=filled]";
-      if(startBool[i]) f << "[shape=triangle]";
-      else if(controlBool[i]) f << "[shape=box]";
+      if(artiSet.find(i)!=artiSet.end()) f << "[color=\".7 .3 1.0\"]" << "[style=filled]";
+      if(startSet.find(i)!=startSet.end()) f << "[shape=triangle]";
+      else if(controlSet.find(i)!=controlSet.end()) f << "[shape=box]";
       f << ";" << '\n';
     }
     pair<edge_iterator, edge_iterator> ei;
@@ -29,67 +29,66 @@ void saveG(const Graph &g, const string &file, vector<bool> &startBool, const ve
 	f.close();
 }
 
-void leGrafo(Graph &g, const string &js, const string &txt, vector<bool> &startBool, vector<bool> &controlBool, map<string, int> &dict){
+void leGrafo(Graph &g, const string &js, const string &txt, set<int> &startSet, set<int>&controlSet, map<string, int> &dict, int &V){
 	ifstream ifs(js);
   Json::Reader reader;
   Json::Value obj;
   reader.parse(ifs, obj); // reader can also read strings
 
-  int k=0;//sera o indice de cada vertice no grafo
+  //V sera o indice de cada vertice no grafo, e no fim representara a quantidade total de vertices
   for(Json::Value val : obj["rows"]){//para cada vertice do json
     string u = val["fromGlobalId"].asString();
     string v = val["toGlobalId"].asString();
-    if(dict.insert(make_pair(u, k)).second){//se o vertice do qual a aresta parte ainda nao tiver sido adicionado ao dict
+    if(dict.insert(make_pair(u, V)).second){//se o vertice do qual a aresta parte ainda nao tiver sido adicionado ao dict
        boost::add_vertex(g);
-       k++;
+       V++;
     }
-    if(dict.insert(make_pair(v, k)).second){//se o vertice no qual a aresta chega ainda nao tiver sido adicionado ao dict
+    if(dict.insert(make_pair(v, V)).second){//se o vertice no qual a aresta chega ainda nao tiver sido adicionado ao dict
       boost::add_vertex(g);
-      k++;
+      V++;
     }
     boost::add_edge(dict[u],dict[v],g);
   }
 
-	controlBool = vector<bool>(k,false);
   for(Json::Value val : obj["controllers"]){//para cada controlador
-    if(dict.insert(make_pair(val["globalId"].asString(), k)).second){//se ele ainda nao tiver sido adicionado ao dict
+    if(dict.insert(make_pair(val["globalId"].asString(), V)).second){//se ele ainda nao tiver sido adicionado ao dict
       boost::add_vertex(g);
-      k++;
+      V++;
     }
-    controlBool[dict[val["globalId"].asString()]] = true;
+    controlSet.insert(dict[val["globalId"].asString()]);
 	}
 
   ifstream startingpoints(txt);
-	startBool = vector<bool>(k,false);
   while(true){//para cada starting point
     string st;
     startingpoints >> st;
-    if(dict.insert(make_pair(st, k)).second){
+    if(dict.insert(make_pair(st, V)).second){
       boost::add_vertex(g);
-      k++;
+      V++;
     }
-    startBool[dict[st]] = true;
+    startSet.insert(dict[st]);
     if(startingpoints.eof())break;
   }
 }
 
-void addAux(Graph &g, const vector<bool> &startBool, const vector<bool> &controlBool){
-	for(int i=0; i<startBool.size(); i++)
-		if(startBool[i])
-			for(int j=0; j<controlBool.size(); j++)
-				if(controlBool[j]) boost::add_edge(i,j,g);
+void addAux(Graph &g, const set<int> &startSet, const set<int> &controlSet){
+
+	set<int>::iterator it1, it2;
+	for(it1=startSet.begin(); it1!=startSet.end(); it1++)
+		for(it2=controlSet.begin(); it2!=controlSet.end(); it2++)
+			boost::add_edge((*it1),(*it2),g);
 }
 
 int main(int argc, char **argv){
   map<string, int>dict;
   Graph g;
-	vector<bool>controlBool;//vetor que indica quais vertices sao controladores
-	vector<bool>startBool;//vetor que indica quais vertices sao starting points
-	leGrafo(g,argv[1],argv[2],controlBool,startBool,dict);
-	addAux(g,startBool,controlBool);
-  vector<vertex> arti;
-  vector<bool> artiBool(startBool.size(), false);
-  boost::articulation_points(g, back_inserter(arti));
-  for(int i=0; i<arti.size(); i++) artiBool[(int)arti[i]]=true;
-  saveG(g,"sampleDataset1.dot",startBool,controlBool,artiBool);
+	int V=0;//representara o indice de cada vertice, e no final representara o numero total de vertices
+	set<int>controlSet;//set que indica quais vertices sao controladores
+	set<int>startSet;//set que indica quais vertices sao starting points
+	leGrafo(g,argv[1],argv[2],controlSet,startSet,dict,V);
+	//addAux(g,startSet,controlSet);
+  vector<vertex> artVec;
+  set<int> artiSet;
+  boost::articulation_points(g, back_inserter(artVec)); for(int i=0; i<artVec.size(); i++) artiSet.insert((int)artVec[i]);
+  saveG(g,"sampleDataset1.dot",startSet,controlSet,artiSet,V);
  }
