b5f26b6c20888ef6589dbeed3f40016e8aeedf68
RicsonV <ricsonluiz@yahoo.com.br>
2018-11-21 12:17:14 -0200

    may the force be with u

--- random.cpp
+++ random.cpp
@@ -1,75 +1,77 @@
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <vector>
 #include <stdlib.h>
-#include "Graph.h"
+#include <algorithm>
+#include <jsoncpp/json/json.h>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/biconnected_components.hpp>
+#include "boost/graph/graph_traits.hpp"
 
 using namespace std;
-
-void tarjanUT(int u, const vector<list<int> > &adj, vector<int> &dfsParent, vector<int> &dfsNum,
-					 vector<int> &dfsLow, vector<bool> &isArticulation,	int &dfsCount, int &rootChildren, int dfsRoot) {
-	dfsLow[u] = dfsNum[u] = dfsCount++; // dfsLow[u] <= dfsNum[u]
-	for(list<int>::const_iterator it = adj[u].begin(); it!=adj[u].end(); it++) {
-		//vertices nao descobertos tem tempo de descoberto (dfsNum) 0
-		if (dfsNum[(*it)] == 0) {
-				dfsParent[(*it)] = u; //pai de v e' u (v foi descoberto por u)
-			if (u == dfsRoot) //caso especial... precisamos contar quantas vezes descobrimos vertices a partir da raiz para descobrir se raiz e' articulacao...
-				rootChildren++;
-
-			tarjanUT((*it),adj,dfsParent,dfsNum,dfsLow,isArticulation,dfsCount,rootChildren,dfsRoot);
-
-			if (dfsLow[(*it)] >= dfsNum[u]) //apos visitar v recursivamente, conseguimos chegar em um vertice em v ou antes passando por u? (sem voltar pela aresta v,u)
-				isArticulation[u] = true; // store this information first
-			dfsLow[u] = min(dfsLow[u], dfsLow[(*it)]); // update dfsLow[u]
-		}
-		else if ((*it) != dfsParent[u]) //cheguei de volta em um vertice ja descoberto... se nao passei pela aresta v,u de volta --> ciclo...
-			dfsLow[u] = min(dfsLow[u], dfsNum[(*it)]); //atualize dfsLow...
-	}
-}
-
-void tarjan(const vector<list<int> > &adj, vector<bool> &isArticulation) {
-	int nv = adj.size();
-	vector<int> dfsParent(nv,-1);
-	vector<int> dfsLow(nv,0);
-	vector<int> dfsNum(nv,0);
-	int dfsCount = 0;
-
-	//para multiplos CCs ...
-	for(int i=0; i<nv; i++) {
-		if(dfsNum[i]==0) {
-			int rootChildren = 0;//resete para cada CC!!
-			tarjanUT(i,adj,dfsParent,dfsNum,dfsLow,isArticulation,dfsCount,rootChildren,i);
-			isArticulation[i] = rootChildren>1;
-		}
-	}
-}
-
+typedef boost::adjacency_list<boost::listS,boost::vecS,boost::undirectedS> Graph;
+typedef boost::graph_traits<Graph>::vertex_descriptor vertex;
+typedef boost::graph_traits<Graph>::edge_iterator edge_iterator;
 
 void geraGrafo(Graph &g, int v, int e) {
-	g = Graph(v);
+	vector<pair<int,int> > arestas;
 	srand(time(NULL));
-	int currE = g.E();
-	for(int i=0; i<e; i++) {
+	for(int i=0; i<v; i++)boost::add_vertex(g);
+	for(int i=0; i<e;) {
 		int x = rand()%v;
 		int y = rand()%v;
-		g.insert(x,y);
-		if(g.E()==currE) i--;
+		if(x!=y && find(arestas.begin(),arestas.end(),make_pair(x,y))==arestas.end() && find(arestas.begin(),arestas.end(),make_pair(y,x))==arestas.end()){
+			boost::add_edge(x,y,g);
+			arestas.push_back(make_pair(x,y));
+			i++;
+		}
 	}
+}
+
+void saveG(const Graph &g, const string &file, vector<bool> &startBool, vector<bool>&controlBool, vector<bool>&arti){
+	  ofstream f(file.c_str(),ios::out);
+		f << "graph G{ " << '\n';
+		for(int i=0;i<startBool.size();i++){
+			f << i;
+      if(arti[i]) f << "[color=\".7 .3 1.0\"]" << "[style=filled]";
+      if(startBool[i]) f << "[shape=triangle]";
+      else if(controlBool[i]) f << "[shape=box]";
+      f << ";" << '\n';
+    }
+    pair<edge_iterator, edge_iterator> ei;
+    for(ei = boost::edges(g); ei.first != ei.second; ++ei.first)
+		  f << source(*ei.first, g) << " -- " << target(*ei.first, g) << ";\n";
+	f << "}" <<endl;
+	f.close();
+}
+
+void addAux(Graph &g, const vector<bool> &startBool, const vector<bool> &controlBool){
+	for(int i=0; i<startBool.size(); i++)
+		if(startBool[i])
+			for(int j=0; j<controlBool.size(); j++)
+				if(controlBool[j]) boost::add_edge(i,j,g);
 }
 
 int main(int argc, char **argv){
-	int v = atoi(argv[2]);
-	Graph g(v);
-	geraGrafo(g, v, atoi(argv[3]));
+	int v = atoi(argv[1]);
+	Graph g;
+	geraGrafo(g, v, atoi(argv[2]));
 
-	vector<bool>start(v);
-	vector<bool>control(v);
-	vector<bool>sol(v); for(int i=0; i<v; i++){sol[i] = false; control[i] = false; start[i] = false;}
+	vector<bool>startBool(v, false);
+	vector<bool>controlBool(v, false);
 
-	/*start[1] = true;
-	control[v-1] = true;
-	control[v-3] = true;*/
-	tarjan(g.adj, sol);
+	srand(time(NULL));
+	startBool[rand()%v] = true;
+	startBool[rand()%v] = true;
+	controlBool[rand()%v] = true;
+	controlBool[rand()%v] = true;
 
-	g.save(argv[1], start, control, sol);
+	vector<vertex> arti;
+	vector<bool> artiBool(v, false);
+	boost::articulation_points(g, back_inserter(arti));
+	for(int i=0; i<arti.size(); i++) artiBool[(int)arti[i]]=true;
+	addAux(g,startBool,controlBool);
+
+  saveG(g,"randomG.dot", startBool, controlBool, artiBool);
 }
--- test2.cpp
+++ test2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <map>
 #include <fstream>
+#include <map>
 #include <algorithm>
 #include <jsoncpp/json/json.h>
 #include <boost/graph/adjacency_list.hpp>
@@ -12,17 +12,16 @@ typedef boost::adjacency_list<boost::listS,boost::vecS,boost::undirectedS> Graph
 typedef boost::graph_traits<Graph>::vertex_descriptor vertex;
 typedef boost::graph_traits<Graph>::edge_iterator edge_iterator;
 
-void saveG(const Graph &g, const string &file, vector<bool> &start, vector<bool>&control, vector<bool>&art){
+void saveG(const Graph &g, const string &file, vector<bool> &startBool, const vector<bool>&controlBool, const vector<bool>&arti){
 	  ofstream f(file.c_str(),ios::out);
 		f << "graph G{ " << '\n';
-		for(int i=0;i<start.size();i++){
+		for(int i=0;i<startBool.size();i++){
 			f << i;
-      if(art[i]) f << "[color=\".7 .3 1.0\"]" << "[style=filled]";
-      if(start[i]) f << "[shape=triangle]";
-      else if(control[i]) f << "[shape=box]";
+      if(arti[i]) f << "[color=\".7 .3 1.0\"]" << "[style=filled]";
+      if(startBool[i]) f << "[shape=triangle]";
+      else if(controlBool[i]) f << "[shape=box]";
       f << ";" << '\n';
     }
-    //pair<edge_iterator, edge_iterator> ei = boost::edges(g);
     pair<edge_iterator, edge_iterator> ei;
     for(ei = boost::edges(g); ei.first != ei.second; ++ei.first)
 		  f << source(*ei.first, g) << " -- " << target(*ei.first, g) << ";\n";
@@ -30,10 +29,8 @@ void saveG(const Graph &g, const string &file, vector<bool> &start, vector<bool>
 	f.close();
 }
 
-int main(){
-  map<string, int>dict;
-  Graph g;
-  ifstream ifs("SampleDataset1/SampleDataset1.json");
+void leGrafo(Graph &g, const string &js, const string &txt, vector<bool> &startBool, vector<bool> &controlBool, map<string, int> &dict){
+	ifstream ifs(js);
   Json::Reader reader;
   Json::Value obj;
   reader.parse(ifs, obj); // reader can also read strings
@@ -53,31 +50,46 @@ int main(){
     boost::add_edge(dict[u],dict[v],g);
   }
 
-  vector<bool>control(k,false);//vetor que indica quais vertices sao controladores
+	controlBool = vector<bool>(k,false);
   for(Json::Value val : obj["controllers"]){//para cada controlador
     if(dict.insert(make_pair(val["globalId"].asString(), k)).second){//se ele ainda nao tiver sido adicionado ao dict
       boost::add_vertex(g);
       k++;
     }
-    control[dict[val["globalId"].asString()]] = true;
-  }
+    controlBool[dict[val["globalId"].asString()]] = true;
+	}
 
-  vector<bool>start(k,false);//vetor que indica quais vertices sao starting points
-  ifstream startingpoints("SampleDataset1/startingpoints.txt");
-  while(true){
+  ifstream startingpoints(txt);
+	startBool = vector<bool>(k,false);
+  while(true){//para cada starting point
     string st;
     startingpoints >> st;
     if(dict.insert(make_pair(st, k)).second){
       boost::add_vertex(g);
       k++;
     }
-    start[dict[st]] = true;
+    startBool[dict[st]] = true;
     if(startingpoints.eof())break;
   }
+}
 
-  vector<vertex> art_pointsAUX;
-  vector<bool> art_points(k, false);
-  boost::articulation_points(g, back_inserter(art_pointsAUX));
-  for(int i=0; i<art_pointsAUX.size(); i++) art_points[(int)art_pointsAUX[i]]=true;
-  saveG(g,"vaidamerda.dot",start,control,art_points);
+void addAux(Graph &g, const vector<bool> &startBool, const vector<bool> &controlBool){
+	for(int i=0; i<startBool.size(); i++)
+		if(startBool[i])
+			for(int j=0; j<controlBool.size(); j++)
+				if(controlBool[j]) boost::add_edge(i,j,g);
+}
+
+int main(int argc, char **argv){
+  map<string, int>dict;
+  Graph g;
+	vector<bool>controlBool;//vetor que indica quais vertices sao controladores
+	vector<bool>startBool;//vetor que indica quais vertices sao starting points
+	leGrafo(g,argv[1],argv[2],controlBool,startBool,dict);
+	addAux(g,startBool,controlBool);
+  vector<vertex> arti;
+  vector<bool> artiBool(startBool.size(), false);
+  boost::articulation_points(g, back_inserter(arti));
+  for(int i=0; i<arti.size(); i++) artiBool[(int)arti[i]]=true;
+  saveG(g,"sampleDataset1.dot",startBool,controlBool,artiBool);
  }
