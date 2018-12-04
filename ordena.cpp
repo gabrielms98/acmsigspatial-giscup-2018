8b79125114fe769db15a02b49920b660bedbe916
Gabriel M Silva <gabriel.silva@nobugs.com.br>
2018-12-04 16:17:16 -0200

    final

--- ordena.cpp
+++ ordena.cpp
@@ -9,18 +9,31 @@
 using namespace std;
 
 int main(int argc, char **argv){
-  ifstream ifs(argv[1]);
-  string st;
-  vector<string> vec;
+  ifstream ifs1(argv[1]);
+  ifstream ifs2(argv[2]);
+  string st1;
+  string st2;
+  vector<string> vec1;
+  vector<string> vec2;
   while(true){
-    if(ifs.eof()) break;
-    ifs >> st;
-    vec.push_back(st);
+    if(ifs1.eof() && ifs2.eof()) break;
+    ifs1 >> st1;
+    vec1.push_back(st1);
+    ifs2 >> st2;
+    vec2.push_back(st2);
   }
 
-  sort( vec.begin(), vec.end() );
-  vec.erase( unique( vec.begin(), vec.end() ), vec.end() );
+  sort( vec1.begin(), vec1.end() );
+  vec1.erase( unique( vec1.begin(), vec1.end() ), vec1.end() );
+  sort( vec2.begin(), vec2.end() );
+  vec2.erase( unique( vec2.begin(), vec2.end() ), vec2.end() );
 
-  for(string i : vec) cout << i << endl;
+  ofstream of1("ourOrd.txt");
+  ofstream of2("expOrd.txt");
+
+  for(string i : vec1) of1 << i << '\n';
+  for(string i : vec2) of2 << i << '\n';
+
+  cout << ((vec1==vec2) ? "True\n" : "False\n");
 
 }
