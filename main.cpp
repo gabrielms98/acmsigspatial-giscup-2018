efee3e9689a1e361afbd000fa8064fdf41d8e40d
Gabriel M Silva <gmbiel1@gmail.com>
2019-05-30 19:33:06 -0300

    README

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include "boost/graph/graph_traits.hpp"
 #include <boost/foreach.hpp>
 #include <string>
-// #include <omp.h>
 #include "include/rapidjson/document.h"
 
 using namespace std;
@@ -25,37 +24,24 @@ typedef boost::graph_traits<Graph>::edge_iterator edge_iterator;//iterador que i
 typedef boost::graph_traits<Graph>::vertex_descriptor vertex_descriptor;//funciona como um ponteiro para vertices do grafo
 typedef boost::graph_traits<Graph>::edge_descriptor edge_descriptor;//funciona como um ponteiro para arestas do grafo
 
-void leGrafo(Graph &g, const string &js, const string &txt, vector<bool> &startVec, vector<bool>&controlVec, map<string, int> &vDictIN, map<int, string> &vDictOUT, multimap<string, int> &eDictIN, multimap<string, pair<int,int> > &edgesMapIN, map<pair<int,int>, string> &edgesMapOUT, int &V, int &E){
+//funcao que lera o grafo a partir do json:
+void leGrafo(Graph &g, const string &js, const string &txt, vector<bool> &startVec, vector<bool>&controlVec, map<string, int> &vDictIN, map<int, string> &vDictOUT, multimap<string, pair<int,int> > &edgesMapIN, map<pair<int,int>, string> &edgesMapOUT, int &V){
   rapidjson::Document doc;
   doc.Parse(js.c_str());
-
-  assert(doc.IsObject());
-  assert(doc.HasMember("rows"));
-
   auto &rows = doc["rows"];
-  assert(rows.IsArray());
   for(auto &obj : rows.GetArray()){
-    assert(obj.IsObject());
-    assert(obj["toGlobalId"].IsString());
-    assert(obj["fromGlobalId"].IsString());
-    assert(obj["viaGlobalId"].IsString());
-
     string u = obj["fromGlobalId"].GetString();//vertice de partida
     string v = obj["toGlobalId"].GetString();//vertice de chegada
 		string e = obj["viaGlobalId"].GetString();//via
-
     if(vDictIN.insert(make_pair(u,V)).second){//para o vertice from, se ele ainda nao tiver sido inserido:
-      boost::add_vertex(g);
+      boost::add_vertex(g);//criamos um vertice solto no grafo g
       vDictOUT.insert(make_pair(V++,u));//inserimos o vertice no dicionario de saida e incrementamos a variavel de indice
     }
-
     if(vDictIN.insert(make_pair(v,V)).second){//para o vertice to, se ele ainda nao tiver sido inserido:
-      boost::add_vertex(g);
+      boost::add_vertex(g);//criamos um vertice solto no grafo g
       vDictOUT.insert(make_pair(V++,v));//inserimos o vertice no dicionario de saida e incrementamos a variavel de indice
     }
-
-    //teremos que inserir a aresta de qualquer forma, pois nao ha arestas repetidas no json. Incrementamos a variavel de indice de aresta
-    eDictIN.insert(make_pair(e,E++));//insira a aresta no dicionario para indice de aresta
+    //teremos que inserir a aresta de qualquer forma, pois nao ha arestas repetidas no json.
     boost::add_edge(vDictIN[u], vDictIN[v], g);//ligue a aresta ao vertice u e ao vertice v
     edgesMapIN.insert(make_pair(e, make_pair(vDictIN[u], vDictIN[v])));//inserimos a aresta no dicionario de entrada, de string para pair<int,int>
     edgesMapOUT.insert(make_pair(make_pair(vDictIN[u], vDictIN[v]), e));//inserimos a aresta no dicionario de saida, de pair<int,int> para string
@@ -64,9 +50,7 @@ void leGrafo(Graph &g, const string &js, const string &txt, vector<bool> &startV
   controlVec = vector<bool>(V,false);//vector que informara' quais vertices sao controladores
   startVec = vector<bool>(V,false);//vector que informara' quais vertices sao starting points
 
-  assert(doc.HasMember("controllers"));
   auto &control = doc["controllers"];
-  assert(control.IsArray());
   for(auto &val : control.GetArray()){
     string c = val["globalId"].GetString();
     controlVec[vDictIN[c]] = true;//setamos o controlvec na posicao daquele vertice para true
@@ -78,75 +62,71 @@ void leGrafo(Graph &g, const string &js, const string &txt, vector<bool> &startV
     string st;
     startingpoints >> st;
     if(st.empty()) break;
-    if(eDictIN.find(st)!=eDictIN.end()){//se o starting point for uma aresta
-      boost::add_vertex(g);//criamos um vertice auxiliar que ira representa'-la
-      multimap<string, pair<int,int> >::iterator it = edgesMapIN.find(st);
-      int u = (*it).second.first;
-      int w = (*it).second.second;
-      boost::remove_edge(u,w,g);//removemos a aresta entre u e w
+    if(edgesMapIN.find(st)!=edgesMapIN.end()){//se o starting point for uma aresta:
+      boost::add_vertex(g);//criamos um vertice auxiliar que ira representa-la
+      multimap<string, pair<int,int> >::iterator it = edgesMapIN.find(st);//encontramos a aresta que possui o id inserido (st)
+      int u = (*it).second.first;//:
+      int w = (*it).second.second;//pegamos os dois vertices adjacentes a ela
+      boost::remove_edge(u, w, g);//removemos a aresta (entre u e w)
       boost::add_edge(V, u, g);//:
-      boost::add_edge(V, w, g);//conectamos o novo vertice aos dois vertices aos quais a aresta que e' starting point estava conectada
+      boost::add_edge(V, w, g);//conectamos o novo vertice aos dois vertices aos quais a aresta estava conectada
       vDictIN.insert(make_pair(st, V));//:
       vDictOUT.insert(make_pair(V,st));//o inserimos no dicionario de vertices normalmente, porem com o nome da aresta
-      startVec.push_back(true);//informamos que o vertice e' um starting point e incrementamos a variavel de indice
-      V++;
     }
-    else{//se o starting point for um vertice
-      startVec[vDictIN[st]] = true;
-    }
+    else//se o starting point for um vertice
+      startVec[vDictIN[st]] = true;//apenas atribua true ao startVec naquela posicao
+
     if(startingpoints.eof()) break;
   }
 }
 
+//funcao que adiciona as arestas auxiliares, cruciais na resolucao do problema:
 void addAux(Graph &g, const vector<bool> &startVec, const vector<bool> &controlVec, const vector<int> &cc, int numCC, set<edge_descriptor> &auxEdges, int &V){
   boost::add_vertex(g);//criamos um vertice que sera ligado a todos os starting points
   int s = V++;//pegamos o indice dele e incrementamos a variavel de indice
-  bool entrouS=false;//indica se teve pelo menos um starting point
-  // #pragma omp parallel for//utilizado para otimizacao
-  for(int i=0; i<V-1; i++)//para cada vertice do CC (tirando o que acabou de ser criado (por isso o "-1") ), se ele for starting point, conecte-o ao vertice que acabou de ser criado (s)
-    if(startVec[i]&&cc[i]==numCC){//se o vertice for starting point e for do componente conexo passado como argumento, crie a aresta.
-      entrouS=true;
-      add_edge(i, s, g);//adiciona uma aresta entre tal starting point e o vertice que foi criado
-    }
-
-  boost::add_vertex(g);//criamos um vertice que sera ligado a todos os starting points
+  boost::add_vertex(g);//criamos um vertice que sera ligado a todos os controllers
   int c = V++;//pegamos o indice dele e incrementamos a variavel de indice
-  bool entrouC=false;//indica se teve pelo menos um controlador
-  // #pragma omp parallel for//otimizacao
-  for(int i=0; i<V-2; i++)//para cada vertice do grafo(tirando os que acabaramde ser criados (por isso o "-2") ), se ele for controller, conecte-o ao vertice que acabou de ser criado (c)
-    if(controlVec[i]&&cc[i]==numCC){//se o vertice for controller e for do componente conexo passado como argumento, crie a aresta.
-      entrouC=true;
-      add_edge(i, c, g);//adiciona uma aresta entre tal controller e o vertice que foi criado
-     }
+  bool encontrouS=false;//indica se foi encontrado pelo menos um starting point
+  bool encontrouC=false;//indica se foi encontrado pelo menos um controller
 
-  if(entrouS&&entrouC){//se tiver encontrado pelo menos um starting point e um controller
+  int tam = startVec.size();//numero total de vertices
+  for(int i=0; i<tam; ++i){//para cada vertice do CC (tirando os dois que acabaram de ser criados)
+    if(startVec[i]&&cc[i]==numCC){//se o vertice for starting point e for do componente conexo passado como argumento, crie a aresta conectando ele ao starting point principal (s)
+      encontrouS=true;
+      add_edge(i, s, g);//adiciona uma aresta entre tal starting point e o principal (s)
+    }
+    if(controlVec[i]&&cc[i]==numCC){//se o vertice for controller e for do componente conexo passado como argumento, crie a aresta conectando ele ao controller principal (c)
+      encontrouC=true;
+      add_edge(i, c, g);//adiciona uma aresta entre tal controller e o principal (c)
+    }
+  }
+  if(encontrouS&&encontrouC){//se tiver encontrado pelo menos um starting point e um controller
     add_edge(s, c, g);//conecte os dois vertices auxiliares s e c.
-    pair<edge_descriptor, bool> auxPair = edge(vertex_descriptor(s), vertex_descriptor(c), g);//o first de auxPair ira armazenar um edge descriptor para a aresta, e isso sera armazenado no set de descriptors das arestas auxiliares
-    auxEdges.insert(auxPair.first);//insira tal descriptor no container de arestas auxiliares
+    pair<edge_descriptor, bool> auxPair = edge(vertex_descriptor(s), vertex_descriptor(c), g);//o "first" de auxPair ira armazenar um edge descriptor (semelhante a um ponteiro) para a aresta, e isso sera armazenado em auxEdges
+    auxEdges.insert(auxPair.first);//armazene o descriptor da aresta auxiliar
   }
 }
 
 int main(int argc, char **argv){
   string str;//string que recebera o conteudo do json, para ser lida pelo rapidjson na funcao leGrafo
-  ifstream in(argv[1]);
-  getline(in,str,(char)-1);
-  Graph g;
+  ifstream in(argv[1]);//arquivo json a ser lido
+  getline(in,str,(char)-1);//converta todo o json para string e armazene em str
+
+  Graph g;//criacao do grafo
   map<string, int> vDictIN;//dicionario que ira converter o nome de cada vertice para seu indice
   map<int, string> vDictOUT;//dicionario que ira converter o indice de cada vertice para seu nome
-  multimap<string, int> eDictIN;//dicionario que ira converter o nome de cada aresta para seu indice
   multimap<string, pair<int,int> > edgesMapIN;//dicionario que ira converter o nome de cada aresta para um pair com seus vertices
-  map<pair<int,int>, string> edgesMapOUT;//dicionario que ira converter os vertices da aresta para seu nome
+  map<pair<int,int>, string> edgesMapOUT;//dicionario que ira converter o pair com dois vertices para o nome da aresta entre eles
+
   ofstream out(argv[3]);
 	int V=0;//representara o indice de cada vertice
-  int E=0;//representara o indice de cada aresta
   vector<bool>controlVec;//vector que indica quais vertices sao controladores
 	vector<bool>startVec;//vector que indica quais vertices sao starting points
 
-  leGrafo(g,str,argv[2],startVec,controlVec,vDictIN,vDictOUT,eDictIN,edgesMapIN,edgesMapOUT,V,E);
+  leGrafo(g,str,argv[2],startVec,controlVec,vDictIN,vDictOUT,edgesMapIN,edgesMapOUT,V);
   vector<int>cc(V);//armazena, no indice de cada vetor, o componente conexo ao qual ele pertence
   set<int> ccNums;//armazena o numero dos componentes conexos
   boost::connected_components(g, &cc[0]);//funcao que armazena em cc o numero do componente conexo de cada vertice
-  // #pragma omp parallel for
   for(int i=0; i<V; i++)//armazena todos os numeros de componentes conexos em ccNums
     ccNums.insert(cc[i]);
 
@@ -159,15 +139,15 @@ int main(int argc, char **argv){
   boost::biconnected_components(g, component);//armazena em component o numero do componente biconectado de cada aresta
 
   set<int> solComps;//armazena o numero dos componentes biconectados que fazem parte da solucao
-  BOOST_FOREACH(edge_descriptor ei, set<edge_descriptor>(auxEdges)){//para cada edge_descriptor em auxEdges,
+  BOOST_FOREACH(edge_descriptor ei, set<edge_descriptor>(auxEdges)){//para cada aresta auxiliar (em auxEdges),
     solComps.insert(component[ei]);//adicione seu componente biconectado 'a solucao
   }
 
   set<int>::iterator solFim = solComps.end();
   for(pair<edge_iterator, edge_iterator> ei = boost::edges(g); ei.first != ei.second; ++ei.first){//para cada aresta do grafo
     if(solComps.find(component[*ei.first])!=solFim){//se ela pertencer a algum dos componentes biconectados da solucao:
-      int u = source(*ei.first,g);
-      int v = target(*ei.first,g);
+      int u = source(*ei.first,g);//indice do vertice de partida
+      int v = target(*ei.first,g);//indice do vertice alvo
       if(!vDictOUT[u].empty()) out << vDictOUT[u] << '\n';//:
       if(!vDictOUT[v].empty()) out << vDictOUT[v] << '\n';//imprima o nome de seus dois vertices
       if(!edgesMapOUT[make_pair(u,v)].empty()) out << edgesMapOUT[make_pair(u,v)] << '\n';//imprima seu proprio nome
